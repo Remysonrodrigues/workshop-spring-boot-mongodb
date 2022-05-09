a1e0793fbf63346b489bcdacee8489b32cc58047
Remyson <remysonrodrigues@gmail.com>
2022-05-09 16:55:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/workshop/mongodb/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.workshop.mongodb.workshopmongodb.services;
+
+import com.workshop.mongodb.workshopmongodb.domain.Post;
+import com.workshop.mongodb.workshopmongodb.repository.PostRepository;
+import com.workshop.mongodb.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
