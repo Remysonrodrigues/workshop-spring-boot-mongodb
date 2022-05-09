4992644c8b87a36ebc10d642e9e5b77ba79d2b8e
Remyson <remysonrodrigues@gmail.com>
2022-05-09 16:48:35 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/workshop/mongodb/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/workshop/mongodb/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.workshop.mongodb.workshopmongodb.resources;
 
+import com.workshop.mongodb.workshopmongodb.domain.Post;
 import com.workshop.mongodb.workshopmongodb.domain.User;
 import com.workshop.mongodb.workshopmongodb.dto.UserDTO;
 import com.workshop.mongodb.workshopmongodb.services.UserService;
@@ -56,6 +57,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
