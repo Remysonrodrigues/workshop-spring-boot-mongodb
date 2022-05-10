c6b057c176884366a6fba9d27eb305f7af4083ed
Remyson <remysonrodrigues@gmail.com>
2022-05-10 11:39:36 -0300

    Consulta com vários critérios

--- src/main/java/com/workshop/mongodb/workshopmongodb/resources/util/URL.java
+++ src/main/java/com/workshop/mongodb/workshopmongodb/resources/util/URL.java
@@ -3,12 +3,26 @@ package com.workshop.mongodb.workshopmongodb.resources.util;
 import org.springframework.stereotype.Component;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Component
 public class URL {
 
     public static String decodeParam(String text){
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 
 }
