a8afcc9e3988ac156dc2b1ffe25937eb971cec28
Remyson <remysonrodrigues@gmail.com>
2022-05-10 10:56:16 -0300

    Consulta simples com query methods

--- src/main/java/com/workshop/mongodb/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/workshop/mongodb/workshopmongodb/repository/PostRepository.java
@@ -4,6 +4,11 @@ import com.workshop.mongodb.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContaining(String text);
+
 }
---
+++ src/main/java/com/workshop/mongodb/workshopmongodb/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.workshop.mongodb.workshopmongodb.resources.util;
+
+import org.springframework.stereotype.Component;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class URL {
+
+    public static String decodeParam(String text){
+        return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+}
